03ebffa7341bbb16d396ff764cb2956df32caa35
javadev <javadev75@gmail.com>
2014-09-16 08:27:49 +0300

    Fix gulp style warnings

--- test/compareStrings.spec.js
+++ test/compareStrings.spec.js
@@ -4,15 +4,15 @@ var expect       = chai.expect;
 /* jshint camelcase: false */
 describe('compareStrings', function () {
   describe('compare', function () {
-    it('should have correct compare for the same strings', function (done) {
+    it('should correct compare for the same strings', function (done) {
        expect(compareStrings('NAME', 'NAME')).to.eql(true);
        done();
     });
-    it('should have correct compare for the modified strings', function (done) {
+    it('should correct compare for the modified strings', function (done) {
        expect(compareStrings('NAEM', 'NAME')).to.eql(true);
        done();
     });
-    it('should have correct compare for the different strings', function (done) {
+    it('should correct compare for the different strings', function (done) {
        expect(compareStrings('NAME1', 'NAME')).to.eql(false);
        done();
     });
