265ca6f7deec5078b66a0f78b59caa381000134e
javadev <javadev75@gmail.com>
2014-10-14 05:40:45 +0300

    Add solution with dictionary

--- test/compareStrings.spec.js
+++ test/compareStrings.spec.js
@@ -1,40 +1,82 @@
-var compareStrings = require('../lib/compareStrings.js');
+var cs = require('../lib/compareStrings.js');
 var chai         = require('chai');
 var expect       = chai.expect;
 /* jshint camelcase: false */
 describe('compareStrings', function () {
-  describe('compare', function () {
+  describe('test compareStrings sort', function () {
     it('should correct compare for the same strings', function (done) {
-       expect(compareStrings('NAME', 'NAME')).to.eql(true);
+       expect(cs.compareStringsSort('NAME', 'NAME')).to.eql(true);
        done();
     });
     it('should correct compare for the modified strings', function (done) {
-       expect(compareStrings('NAEM', 'NAME')).to.eql(true);
+       expect(cs.compareStringsSort('NAEM', 'NAME')).to.eql(true);
        done();
     });
     it('should correct compare for the different strings', function (done) {
-       expect(compareStrings('NAME1', 'NAME')).to.eql(false);
+       expect(cs.compareStringsSort('NAME1', 'NAME')).to.eql(false);
        done();
     });
     it('should correct compare for the different strings', function (done) {
-       expect(compareStrings.bind(null, 'NAME')).to.throw(Error);
+       expect(cs.compareStringsSort('NAME', 'NAME1')).to.eql(false);
        done();
     });
     it('should correct compare for the different strings', function (done) {
-       expect(compareStrings.bind('NAME1', null)).to.throw(Error);
+       expect(cs.compareStringsSort('NNAME', 'NAME1')).to.eql(false);
+       done();
+    });
+    it('should throws exception if first parameter is null', function (done) {
+       expect(cs.compareStringsSort.bind(null, 'NAME')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is null', function (done) {
+       expect(cs.compareStringsSort.bind('NAME1', null)).to.throw(Error);
+       done();
+    });
+    it('should throws exception if first parameter is undefined', function (done) {
+       expect(cs.compareStringsSort.bind(undefined, 'NAME2')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is undefined', function (done) {
+       expect(cs.compareStringsSort.bind('NAME1', undefined)).to.throw(Error);
+       done();
+    });
+  });
+  describe('test compareStrings dictionary', function () {
+    it('should correct compare for the same strings', function (done) {
+       expect(cs.compareStringsDict('NAME', 'NAME')).to.eql(true);
+       done();
+    });
+    it('should correct compare for the modified strings', function (done) {
+       expect(cs.compareStringsDict('NAEM', 'NAME')).to.eql(true);
        done();
     });
     it('should correct compare for the different strings', function (done) {
-       expect(compareStrings.bind(undefined, 'NAME2')).to.throw(Error);
+       expect(cs.compareStringsDict('NAME1', 'NAME')).to.eql(false);
        done();
     });
     it('should correct compare for the different strings', function (done) {
-       expect(compareStrings.bind('NAME1', undefined)).to.throw(Error);
+       expect(cs.compareStringsDict('NAME', 'NAME1')).to.eql(false);
+       done();
+    });
+    it('should correct compare for the different strings', function (done) {
+       expect(cs.compareStringsDict('NNAME', 'NAME1')).to.eql(false);
+       done();
+    });
+    it('should throws exception if first parameter is null', function (done) {
+       expect(cs.compareStringsDict.bind(null, 'NAME')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is null', function (done) {
+       expect(cs.compareStringsDict.bind('NAME1', null)).to.throw(Error);
+       done();
+    });
+    it('should throws exception if first parameter is undefined', function (done) {
+       expect(cs.compareStringsDict.bind(undefined, 'NAME2')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is undefined', function (done) {
+       expect(cs.compareStringsDict.bind('NAME1', undefined)).to.throw(Error);
        done();
     });
   });
 });
-
-//ok(compareStrings('NAME', 'NAME'));
-//ok(compareStrings('NAEM', 'NAME'));
-//false(compareStrings('NAME1', 'NAME'));
