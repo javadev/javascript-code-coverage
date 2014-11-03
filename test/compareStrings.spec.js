fb440172489e06e4702535ae118ae621168e3f74
javadev <javadev75@gmail.com>
2014-11-03 07:42:50 +0200

    Add slice method for string comparition

--- test/compareStrings.spec.js
+++ test/compareStrings.spec.js
@@ -83,4 +83,44 @@ describe('compareStrings', function () {
        done();
     });
   });
+  describe('test compareStrings slice', function () {
+    it('should correct compare for the same strings', function (done) {
+       expect(cs.compareStringsSlice('NAME', 'NAME')).to.eql(true);
+       done();
+    });
+    it('should correct compare for the modified strings', function (done) {
+       expect(cs.compareStringsSlice('NAEM', 'NAME')).to.eql(true);
+       done();
+    });
+    it('should correct compare for the different strings', function (done) {
+       expect(cs.compareStringsSlice('NAME1', 'NAME')).to.eql(false);
+       done();
+    });
+    it('should correct compare for the different strings', function (done) {
+       expect(cs.compareStringsSlice('NAME', 'NAME1')).to.eql(false);
+       done();
+    });
+    it('should correct compare for the different strings', function (done) {
+       expect(cs.compareStringsSlice('NNAME', 'NAME1')).to.eql(false);
+       done();
+    });
+    it('should throws exception if first parameter is null', function (done) {
+       expect(cs.compareStringsSlice.bind(null, 'NAME')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is null', function (done) {
+       expect(cs.compareStringsSlice.bind('NAME1', null)).to.throw(Error);
+       done();
+    });
+    it('should throws exception if first parameter is undefined',
+    function (done) {
+       expect(cs.compareStringsSlice.bind(undefined, 'NAME2')).to.throw(Error);
+       done();
+    });
+    it('should throws exception if second parameter is undefined',
+    function (done) {
+       expect(cs.compareStringsSlice.bind('NAME1', undefined)).to.throw(Error);
+       done();
+    });
+  });
 });
